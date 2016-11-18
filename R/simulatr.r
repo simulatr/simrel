4a7de113d76a04fc3085905cdbdad9d9373c844b
Raju Rimal <therimalaya@gmail.com>
2016-11-18 13:25:39 +0100

    Initial Package with Documentation ready

--- R/simulatr.r
+++ R/simulatr.r
@@ -1,32 +1,36 @@
+#' Validating parameter that is passed into \code{simulatr} function
+#' @param par_list A list of parameters that \code{simulatr} takes
+#' @return A list of validation message either stop message \code{stop_msg} or warning message \code{warn_msg}
+#' @export
 
-validate_parm <- function(parList){
-  list2env(parList, envir = environment())
-  stopMsg <- list()
-  warnMsg <- list()
+validate_param <- function(par_list){
+  list2env(par_list, envir = environment())
+  stop_msg <- list()
+  warn_msg <- list()
 
   ## Critical Error
   if (!all(sapply(list(length(relpos), length(R2)), identical, length(q))))
-    stopMsg$uneql <- "Length of relpos, R2 and q must be equal\n"
+    stop_msg$uneql <- "Length of relpos, R2 and q must be equal\n"
 
   if (!all(sapply(seq_along(q), function(i) q[i] > sapply(relpos, length)[i])))
-    stopMsg$bigPred <- "Number of relevant predictor is smaller than the number of relevant components\n"
+    stop_msg$bigPred <- "Number of relevant predictor is smaller than the number of relevant components\n"
 
   if (!sum(q) < p)
-    stopMsg$smallnvar <- "Number of variables can not be smaller than the number of relevant variables\n"
+    stop_msg$smallnvar <- "Number of variables can not be smaller than the number of relevant variables\n"
 
   if (!max(unlist(relpos)) < p)
-    stopMsg$bigRelpos <- "Relevant Position can not exceed the number of variables\n"
+    stop_msg$bigRelpos <- "Relevant Position can not exceed the number of variables\n"
 
   if (!all(R2 < 1 & R2 > 0))
-    stopMsg$invalidR2 <- "R2 must be between 0 and 1\n"
+    stop_msg$invalidR2 <- "R2 must be between 0 and 1\n"
   if (!is.null(muX) & length(muX) != p) {
-    stopMsg$muXlength <- "Mean of X must have same length as the number of X variables\n"
+    stop_msg$muXlength <- "Mean of X must have same length as the number of X variables\n"
   }
   if (!is.null(muY) & length(muY) != m) {
-    stopMsg$muYlength <- "Mean of Y must have same length as the number of Y variables\n"
+    stop_msg$muYlength <- "Mean of Y must have same length as the number of Y variables\n"
   }
   if (any(duplicated(unlist(ypos)))) {
-    stopMsg$duplicateY <- "Response Space must have unique combination of response variable."
+    stop_msg$duplicateY <- "Response Space must have unique combination of response variable."
   }
 
   ## Warning Conditions
@@ -34,26 +38,66 @@ validate_parm <- function(parList){
     warnMst$noisY <- "Current setting of ypos will produce uninformative response variable."
   }
 
-  valdMsg <- list(stopMsg = stopMsg, warnMsg = warnMsg)
+  valdMsg <- list(stop_msg = stop_msg, warn_msg = warn_msg)
   return(valdMsg)
 }
 
-simrelM <- function(n = 100, p = 15, q = c(5, 4, 3), m = 5,
+#' Simulation of Multivariate Linear Model Data
+#' @param n Number of observations
+#' @param p Number of variables
+#' @param q Vector containing the number of relevant predictor variables for each relevant response components
+#' @param m Number of response variables
+#' @param relpos A list of position of relevant component for predictor variables. The list contains vectors of position index, one vector or each relevant response components
+#' @param gamma A declining (decaying) factor of eigen value of predictors (X). Higher the value of \code{gamma}, the decrease of eigenvalues will be steeper
+#' @param R2 Vector of coefficient of determination (proportion of variation explained by predictor variable) for each relevant response components
+#' @param ntest Number of test observation
+#' @param muX Vector of average (mean) for each predictor variable
+#' @param muY Vector of average (mean) for each response variable
+#' @param ypos List of position of relevant response components that are combined to generate response variable during orthogonal rotation
+#' @return A simrel object with all the input arguments along with following additional items
+#' \item{X}{Simulated predictors}
+#' \item{Y}{Simulated responses}
+#' \item{W}{Simulated predictor components}
+#' \item{Z}{Simulated response components}
+#' \item{beta}{True regression coefficients}
+#' \item{beta0}{True regression intercept}
+#' \item{relPred}{Position of relevant predictors}
+#' \item{testX}{Test Predictors}
+#' \item{testY}{Test Response}
+#' \item{testW}{Test predictor components}
+#' \item{testZ}{Test response components}
+#' \item{minerror}{Minimum model error}
+#' \item{Xrotation}{Rotation matrix of predictor (R)}
+#' \item{Yrotation}{Rotation matrix of response (Q)}
+#' \item{type}{Type of simrel object \emph{univariate} or \emph{multivariate}}
+#' \item{lambda}{Eigenvalues of predictors}
+#' \item{SigmaWZ}{Variance-Covariance matrix of components of response and predictors}
+#' \item{SigmaWX}{Covariance matrix of response components and predictors}
+#' \item{SigmaYZ}{Covariance matrix of response and predictor components}
+#' \item{Sigma}{Variance-Covariance matrix of response and predictors}
+#' \item{RsqW}{Coefficient of determination corresponding to response components}
+#' \item{RsqY}{Coefficient of determination corresponding to response variables}
+#' @keywords simulation, linear model, linear model data
+#' @references Sæbø, S., Almøy, T., & Helland, I. S. (2015). simrel—A versatile tool for linear model data simulation based on the concept of a relevant subspace and relevant predictors. Chemometrics and Intelligent Laboratory Systems, 146, 128-135.
+#' @references Almøy, T. (1996). A simulation study on comparison of prediction methods when only a few components are relevant. Computational statistics & data analysis, 21(1), 87-107.
+#' @export
+
+simulatr <- function(n = 100, p = 15, q = c(5, 4, 3), m = 5,
                     relpos = list(c(1, 2), c(3, 4, 6), c(5, 7)),
                     gamma = 0.6, R2 = c(0.8, 0.7, 0.8),
                     ntest = NULL, muX = NULL, muY = NULL, rho = 0.1,
                     ypos = list(c(1), c(3, 4), c(2, 5))) {
 
   ## Validate Inputs
-  argList <- as.list(environment())
+  arg_list <- as.list(environment())
   ### Make Different Function for this and pass argList to the function
-  val.out <- validate_parm(argList)
-  if (length(val.out$stopMsg) != 0) {
-    stop(paste(sapply(val.out$stopMsg, paste, collapse = '\n')), call. = FALSE)
+  val.out <- validate_param(arg_list)
+  if (length(val.out$stop_msg) != 0) {
+    stop(paste(sapply(val.out$stop_msg, paste, collapse = '\n')), call. = FALSE)
   }
 
-  if (length(val.out$warnMsg) != 0) {
-    warning(paste(sapply(val.out$warnMsg, paste, collapse = '\n')), call. = FALSE)
+  if (length(val.out$warn_msg) != 0) {
+    warning(paste(sapply(val.out$warn_msg, paste, collapse = '\n')), call. = FALSE)
   }
 
   ## Expected Error Messages
@@ -232,6 +276,6 @@ simrelM <- function(n = 100, p = 15, q = c(5, 4, 3), m = 5,
     RsqW = RsqW,
     RsqY = RsqY
   )
-  ret <- `class<-`(append(argList, ret), 'simrel')
+  ret <- `class<-`(append(arg_list, ret), 'simrel')
   return(ret)
 }
