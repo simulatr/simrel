c7483f7921234c253a22dc047c05720443fcd08b
Raju Rimal <therimalaya@gmail.com>
2016-11-18 15:25:11 +0100

    I can build this in emacs

--- R/simulatr.r
+++ R/simulatr.r
@@ -7,7 +7,7 @@ validate_param <- function(par_list){
   list2env(par_list, envir = environment())
   stop_msg <- list()
   warn_msg <- list()
-
+  
   ## Critical Error
   if (!all(sapply(list(length(relpos), length(R2)), identical, length(q))))
     stop_msg$uneql <- "Length of relpos, R2 and q must be equal\n"
@@ -37,7 +37,6 @@ validate_param <- function(par_list){
   if (any(unlist(lapply(ypos, identical, 1:2)))) {
     warnMst$noisY <- "Current setting of ypos will produce uninformative response variable."
   }
-
   valdMsg <- list(stop_msg = stop_msg, warn_msg = warn_msg)
   return(valdMsg)
 }
@@ -55,28 +54,28 @@ validate_param <- function(par_list){
 #' @param muY Vector of average (mean) for each response variable
 #' @param ypos List of position of relevant response components that are combined to generate response variable during orthogonal rotation
 #' @return A simrel object with all the input arguments along with following additional items
-#' \item{X}{Simulated predictors}
-#' \item{Y}{Simulated responses}
-#' \item{W}{Simulated predictor components}
-#' \item{Z}{Simulated response components}
-#' \item{beta}{True regression coefficients}
-#' \item{beta0}{True regression intercept}
-#' \item{relPred}{Position of relevant predictors}
-#' \item{testX}{Test Predictors}
-#' \item{testY}{Test Response}
-#' \item{testW}{Test predictor components}
-#' \item{testZ}{Test response components}
-#' \item{minerror}{Minimum model error}
-#' \item{Xrotation}{Rotation matrix of predictor (R)}
-#' \item{Yrotation}{Rotation matrix of response (Q)}
-#' \item{type}{Type of simrel object \emph{univariate} or \emph{multivariate}}
-#' \item{lambda}{Eigenvalues of predictors}
-#' \item{SigmaWZ}{Variance-Covariance matrix of components of response and predictors}
-#' \item{SigmaWX}{Covariance matrix of response components and predictors}
-#' \item{SigmaYZ}{Covariance matrix of response and predictor components}
-#' \item{Sigma}{Variance-Covariance matrix of response and predictors}
-#' \item{RsqW}{Coefficient of determination corresponding to response components}
-#' \item{RsqY}{Coefficient of determination corresponding to response variables}
+#'     \item{X}{Simulated predictors}
+#'     \item{Y}{Simulated responses}
+#'     \item{W}{Simulated predictor components}
+#'     \item{Z}{Simulated response components}
+#'     \item{beta}{True regression coefficients}
+#'     \item{beta0}{True regression intercept}
+#'     \item{relPred}{Position of relevant predictors}
+#'     \item{testX}{Test Predictors}
+#'     \item{testY}{Test Response}
+#'     \item{testW}{Test predictor components}
+#'     \item{testZ}{Test response components}
+#'     \item{minerror}{Minimum model error}
+#'     \item{Xrotation}{Rotation matrix of predictor (R)}
+#'     \item{Yrotation}{Rotation matrix of response (Q)}
+#'     \item{type}{Type of simrel object \emph{univariate} or \emph{multivariate}}
+#'     \item{lambda}{Eigenvalues of predictors}
+#'     \item{SigmaWZ}{Variance-Covariance matrix of components of response and predictors}
+#'     \item{SigmaWX}{Covariance matrix of response components and predictors}
+#'     \item{SigmaYZ}{Covariance matrix of response and predictor components}
+#'     \item{Sigma}{Variance-Covariance matrix of response and predictors}
+#'     \item{RsqW}{Coefficient of determination corresponding to response components}
+#'     \item{RsqY}{Coefficient of determination corresponding to response variables}
 #' @keywords simulation, linear model, linear model data
 #' @references Sæbø, S., Almøy, T., & Helland, I. S. (2015). simrel—A versatile tool for linear model data simulation based on the concept of a relevant subspace and relevant predictors. Chemometrics and Intelligent Laboratory Systems, 146, 128-135.
 #' @references Almøy, T. (1996). A simulation study on comparison of prediction methods when only a few components are relevant. Computational statistics & data analysis, 21(1), 87-107.
